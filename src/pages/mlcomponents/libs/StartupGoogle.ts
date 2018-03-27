683c2a8b1a94846a33fec6efc52f654f2320fef0
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2018-03-27 12:16:24 -0700

    named css grid areas not working

--- src/pages/mlcomponents/libs/StartupGoogle.ts
+++ src/pages/mlcomponents/libs/StartupGoogle.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+// import {Injectable} from '@angular/core';
 import { MLConfig } from './MLConfig';
 // import { PusherConfig } from './PusherConfig';
 // import { PusherClientService } from '../../../services/pusherclient.service';
