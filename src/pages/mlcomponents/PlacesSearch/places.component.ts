b1d805cdb062881698ae53eeeda19910de1f14a5
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2018-05-03 13:16:56 -0700

    PlacesService is working better than SearchBox.  Check in before cleaning up dead code

--- src/pages/mlcomponents/PlacesSearch/places.component.ts
+++ src/pages/mlcomponents/PlacesSearch/places.component.ts
@@ -6,6 +6,7 @@ import { Component, NgZone, AfterViewInit, ViewChild, ElementRef} from '@angular
 // import {} from '@types/googlemaps';
 // import { MouseEvent } from '@agm/core';
 import { MapInstanceService } from '../../../services/MapInstanceService';
+import 'rxjs/add/operator/toPromise';
 
 declare var google;
 
@@ -27,7 +28,19 @@ export class PlacesSearchComponent implements AfterViewInit {
 
   }
 
-  async placesAsync() {
+  handleError(error) {
+      console.log(error);
+  }
+
+  async placesAsync() : Promise<google.maps.places.PlaceResult[]> {
+      try {
+         console.log('ready to getPlaces in placesAsync');
+         let response = await this.searchBox.getPlaces(); //.toPromise();
+         return response; //json().data as google.maps.places.PlaceResult[];
+      } catch (error) {
+          await this.handleError(error);
+      }
+      /*
       return await new Promise((resolve, reject) => {
           try {
               let places = this.searchBox.getPlaces().toPromise();
@@ -40,6 +53,7 @@ export class PlacesSearchComponent implements AfterViewInit {
               return ex();
           }
       })
+      */
   }
   ngAfterViewInit() {
       // this.input = this.searchBoxRef.nativeElement;
@@ -48,6 +62,8 @@ export class PlacesSearchComponent implements AfterViewInit {
 //
       this.searchBox = new google.maps.places.SearchBox(this.input);
       this.searchBox.addListener("places_changed", () => {
+          var places = [],
+              queryPlaces = {'bounds' : google.maps.LatLngBounds, 'location': google.maps.LatLng, 'query': String};
         // this._ngZone.run(() => {
           console.log('listening');
           console.log(this.searchBox);
@@ -57,17 +73,28 @@ export class PlacesSearchComponent implements AfterViewInit {
           let bnds = mph.getSearchBounds();
           console.log("latest bounds");
           console.debug(bnds);
-          this.searchBox.setBounds(bnds);
+          // this.searchBox.setBounds(bnds);
           // mphmap.addListener('bounds_changed', () => searchBox.setBounds(mph.getSearchBounds()));
-          let places = this.placesAsync(); //this.searchBox.getPlaces();
+          // places = this.searchBox.getPlaces();
+          queryPlaces.bounds = bnds;
+          queryPlaces.location = mphmap.getCenter();
+          queryPlaces.query = this.input.value;
+          let service = new google.maps.places.PlacesService(mphmap);
+          // if (queryPlaces.query !== '') {
+              service.textSearch(queryPlaces, (p) => {
+                  if (p.length != 0) {
+                    mph.placeMarkers(p);
+                  }
+              })
+          //}
           console.log(places);
-          if (places.json().length == 0) {
+          if (places.length == 0) {
             return;
           }
-          else {
-              // this.placeMarkers(mphmap, places);
-              mph.placeMarkers(places);
-          }
+          // else {
+          //     // this.placeMarkers(mphmap, places);
+          //     mph.placeMarkers(places);
+          // }
         // })
       });
 
