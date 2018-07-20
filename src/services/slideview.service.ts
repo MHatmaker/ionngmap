4d984800c15946a0026aaba1143b57c27ea39adf
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2018-07-19 20:03:32 -0700

    Revisions of css to hide hiddenmap and enlarge carousel viewer

--- src/services/slideview.service.ts
+++ src/services/slideview.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SlideViewService {
-    mapcolheight = 400;
+    mapcolheight = 550;
 
     setMapColHeight(h : number) {
         this.mapcolheight = h;
