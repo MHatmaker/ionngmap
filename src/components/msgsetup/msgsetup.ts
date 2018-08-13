e67173f9209102355bb4b1f346797426e7c34712
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2018-08-12 17:36:15 -0700

    clip working, but npm clipboard was not

--- src/components/msgsetup/msgsetup.ts
+++ src/components/msgsetup/msgsetup.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
-import { FormGroup, FormBuilder } from '@angular/forms';
 import { CurrentMapTypeService } from '../../services/currentmaptypeservice';
 import { MapInstanceService } from '../../services/MapInstanceService';
 import { GeoPusherSupport } from '../../pages/mlcomponents/libs/geopushersupport';
@@ -12,18 +11,14 @@ import * as Clipboard from 'clipboard/dist/clipboard.min.js';
 })
 export class MsgsetupComponent {
 
-  private msgsetupgroup : FormGroup;
   private urlCopyField : string;
   private instructionsVisible : boolean = false;
   private mapInstanceService : MapInstanceService;
   private currentMapTypeService : CurrentMapTypeService;
 
-  constructor(public viewCtrl: ViewController, private formBuilder : FormBuilder, private geopush : GeoPusherSupport) {
+  constructor(public viewCtrl: ViewController, private geopush : GeoPusherSupport) {
     console.log('Hello MsgsetupComponent Component');
 
-    this.msgsetupgroup = this.formBuilder.group({
-      urlCopyField: ["url"],
-    });
     let geoPush = geopush.getGeoPusherSupport();
     this.mapInstanceService = geoPush.mapInstanceService;
     this.currentMapTypeService = geoPush.currentMapTypeService;
@@ -53,26 +48,43 @@ export class MsgsetupComponent {
 
       return updtUrl;
   }
+
+  // copyToClipboard(text) {
+  //   if (window.clipboardData && window.clipboardData.setData) {
+  //       // IE specific code path to prevent textarea being shown while dialog is visible.
+  //       return clipboardData.setData("Text", text);
+  //       Window.cl
+  //     }
+  // }
+  copyUrlField(inputElement){
+      inputElement.select();
+      document.execCommand('copy');
+      inputElement.setSelectionRange(0, 0);
+    }
+
   fetchUrl() {
-    // this.urlCopyField = this.assembleUrl();
-    this.msgsetupgroup.controls['urlCopyField'].setValue(this.assembleUrl());
-    console.log(this.msgsetupgroup.controls['urlCopyField'].value);
+    this.urlCopyField = this.assembleUrl();
+
+    console.log(this.urlCopyField);
     this.instructionsVisible = true;
-    let clipboard = new Clipboard('#cpyBtn');
-    clipboard.on('success', (e) => {
-      console.log("copied to clipboard");
-      console.info('Action:', e.action);
-      console.info('Text:', e.text);
-      console.info('Trigger:', e.trigger);
-    });
-    clipboard.on('error', (e) => {
-      console.error('Action:', e.action);
-      console.error('Trigger:', e.trigger);
-    });
+    // let clipboard = new Clipboard('#cpyBtn', {container: document.getElementById('idMsgSetupCard')});
+    // clipboard.text = this.urlCopyField;
+    // clipboard.on('success', (e) => {
+    //   e.clipboardData = this.urlCopyField;
+    //   console.log("copied to clipboard");
+    //   console.info('clipboardData', e.clipboardData);
+    //   console.info('Action:', e.action);
+    //   console.info('Text:', e.text);
+    //   console.info('Trigger:', e.trigger);
+    // });
+    // clipboard.on('error', (e) => {
+    //   console.error('Action:', e.action);
+    //   console.error('Trigger:', e.trigger);
+    // });
   }
 
   logForm(){
-    console.log(this.msgsetupgroup.value.urlCopyField);
+    console.log(this.urlCopyField);
   }
   close() {
       this.viewCtrl.dismiss();
