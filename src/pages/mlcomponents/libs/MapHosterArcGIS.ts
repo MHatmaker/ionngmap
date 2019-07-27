92f63ab9ca2373b9a2b6b300af2a097278a693a3
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2019-07-27 16:04:35 -0700

    restores check on referrerId comparison

--- src/pages/mlcomponents/libs/MapHosterArcGIS.ts
+++ src/pages/mlcomponents/libs/MapHosterArcGIS.ts
@@ -298,7 +298,7 @@ export class MapHosterArcGIS extends MapHoster implements OnInit {
     }
 
     async retrievedClick(clickPt) {
-      if (1) {  // clickPt.referrerId !== this.mlconfig.getUserId()) {
+      if (clickPt.referrerId !== this.mlconfig.getUserId()) {
         const options = {
           url: 'https://js.arcgis.com/4.8/'
         };
