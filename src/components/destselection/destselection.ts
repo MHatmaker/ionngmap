38f6d639c9a716e18107dceb376379d4acbb5c8f
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2020-01-06 14:21:58 -0700

    Fixes font color problem on radio list items

--- src/components/destselection/destselection.ts
+++ src/components/destselection/destselection.ts
@@ -1,58 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { ViewController } from 'ionic-angular';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Destination, DestinationsProvider } from '../../providers/destinations/destinations';
 
 @Component({
   selector: 'destselection',
   templateUrl: 'destselection.html'
 })
-export class DestselectionComponent {
+export class DestselectionComponent implements OnInit {
   public selectedDestination : Destination;
-  public destselgroup : FormGroup;
-  public destinations : Destination[];
-  public useDestination : string;
+  public destinations : Array<Destination>=[];
+  // public useDestination : string;
+  @ViewChild('useDestination', { read: ElementRef }) useDestination : ElementRef;
   // destinations = [
   //   {title : "Same Window", description: "Replace the map in the current window"},
   //   {title : "New Tab", description: "Open the new map in a new tab/slide"},
   //   {title : " New Window", description: "Open the new map in a new window"}
   // ];
 
-  constructor(public viewCtrl: ViewController, public formBuilder : FormBuilder, private destinationsProvider : DestinationsProvider ) {
+  constructor(public viewCtrl: ViewController, private destinationsProvider : DestinationsProvider ) {
     console.log('Hello DestselectionComponent Component');
     this.destinations = destinationsProvider.getDestinations();
     this.selectedDestination = destinationsProvider.previousDestination();
-    this.destselgroup = formBuilder.group({
-      selectedDestination : this.selectedDestination,
-      dests : this.destinations,
-      useDestination : 'Open new map in ' + this.selectedDestination.title
-    });
-    // this.destselgroup.setValue({selectedDestination : this.selectedDestination, useDestination : this.selectedDestination.title, dests: this.destinations})
-    this.destselgroup.controls['dests'].setValue(this.selectedDestination);
-    // this.destselgroup.value = {title:  "Same Window", description : "never mind";}
+    // this.useDestination = 'Open new map in ' + this.selectedDestination.title;
+  }
+
+  ngOnInit() {
+    this.useDestination.nativeElement.innerText = this.selectedDestination.title;
   }
 
   checkDestination(item) {
       // this.selectedDestination = item;
       // console.log(this.destselgroup);
       // this.destselgroup.value['destTitle'] = item.title;
-      item.isChecked = true;
-      this.destselgroup.value['selectedDestination'] = item;
-      this.destselgroup.patchValue({useDestination : 'Open new map in ' + item.title});
-      this.destselgroup.patchValue({dests : item});
-      this.destselgroup.patchValue({selectedDestination : item});
-      // console.log("checkDestination - item " + this.selectedDestination);
-      this.destselgroup.controls['dests'].setValue(item);
-      console.log(this.destselgroup.value);
+      // item.isChecked = true;
+      this.destinationsProvider.clearChecks(item);
+      this.selectedDestination = item;
+      // this.selectedDestination.isChecked = true;
+      // this.useDestination = 'Open new map in ' + item.title;
+      this.useDestination.nativeElement.innerText = this.selectedDestination.description;
+      // this.useDestination.value = 'Open new map in ' + this.selectedDestination.title;
   }
 
   accept() {
-      this.destinationsProvider.preserveDestination(this.destselgroup.value['selectedDestination'])
-      this.viewCtrl.dismiss({destination : this.destselgroup.value['selectedDestination']});
+      this.destinationsProvider.preserveDestination(this.selectedDestination);
+      this.viewCtrl.dismiss({destination : this.selectedDestination});
       // this.viewCtrl.dismiss({destination : this.selectedDestination.title});
   }
   logForm(){
-    console.log(this.destselgroup.value)
   }
   cancel() {
       this.viewCtrl.dismiss({destination : "cancel"});
--- src/providers/destinations/destinations.ts
+++ src/providers/destinations/destinations.ts
@@ -15,7 +15,7 @@ export class Destination {
 export class DestinationsProvider {
 
   private isFirstUse : boolean = true;
-  private destinations : Destination[] = [
+  private destinations : Array<Destination> = [
     {title : "Same Window", description: "Replace the map in the current window", isChecked : true, id : 0, color : "primary"},
     {title : "New Tab", description: "Open the new map in a new tab/slide", isChecked : false, id : 1, color : "secondary"},
     {title : " New Window", description: "Open the new map in a new window", isChecked : false, id : 2, color : "secondary"}
@@ -25,7 +25,7 @@ export class DestinationsProvider {
 
   constructor() {
     if (this.isFirstUse) {
-      this.currentDestination = new Destination("SameWindow", "Replace the map in the current window",  false, 0);
+      this.currentDestination = this.destinations[0]; //new Destination("SameWindow", "Replace the map in the current window",  false, 0);
       this.isFirstUse = false;
     } else {
       this.currentDestination = new Destination(this.currentDestination.title, this.currentDestination.description,
@@ -46,6 +46,15 @@ export class DestinationsProvider {
           } else {
             this.destinations[i].isChecked = false;
             this.destinations[i].color = "secondary";
+          }
+      }
+  }
+  clearChecks(dest : Destination) {
+      for (let i in this.destinations) {
+          if(this.destinations[i].id = dest.id) {
+            this.destinations[i].isChecked = true;
+          } else {
+            this.destinations[i].isChecked = false;
           }
       }
   }
