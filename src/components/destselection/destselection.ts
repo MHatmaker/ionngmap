1985a6c3143bb1da553160177bc1231fd8d91968
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2020-01-08 16:38:53 -0700

    uses NgZone rather than ViewChild to refresh radio buttons, clearChecks method not needed

--- src/components/destselection/destselection.ts
+++ src/components/destselection/destselection.ts
@@ -9,7 +9,8 @@ import { Destination, DestinationsProvider } from '../../providers/destinations/
 export class DestselectionComponent implements OnInit {
   public selectedDestination : Destination;
   public destinations : Array<Destination>=[];
-  @ViewChild('useDestination', { read: ElementRef }) useDestination : ElementRef;
+  public useDestination : string;
+  // @ViewChild('useDestination', { read: ElementRef }) useDestination : ElementRef;
 
   constructor(public viewCtrl: ViewController, private destinationsProvider : DestinationsProvider,
     private zone:NgZone) {
@@ -19,15 +20,17 @@ export class DestselectionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.useDestination.nativeElement.innerText = this.selectedDestination.title;
-    this.destinationsProvider.clearChecks(this.selectedDestination);
+    // this.useDestination.nativeElement.innerText = this.selectedDestination.title;
+    this.useDestination = this.selectedDestination.title;
+    // this.destinationsProvider.clearChecks(this.selectedDestination);
   }
 
   checkDestination(item) {
       this.selectedDestination = item;
-      this.useDestination.nativeElement.innerText = this.selectedDestination.description;
+      // this.useDestination.nativeElement.innerText = this.selectedDestination.description;
       this.zone.run(() => {
-          this.destinationsProvider.clearChecks(item);
+          // this.destinationsProvider.clearChecks(item);
+          this.useDestination = this.selectedDestination.description;
       });
   }
 
